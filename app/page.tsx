9f33024d28047ebe20226bad01d1c2077fd0733b
NullClock <146401816+NullClock@users.noreply.github.com>
2024-09-16 17:45:43 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,145 @@
+'use client';
+
+import Image from 'next/image';
+import Head from 'next/head';
+// @ts-ignore
+import { en, pt } from './ptbr.ts';
+import { useState } from 'react';
+
+export default function Home() {
+  const [lang, setLang] = useState(en);
+
+  const toggleLang = () => {
+    setLang(lang === pt ? en : pt);
+  };
+
+  const changeLangBTN = (
+    <button
+      onClick={toggleLang}
+      className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mr-4"
+    >
+      {lang.toggle}
+    </button>
+  );
+
+  return (
+    <main className="max-w-5xl mx-auto p-4 md:p-6 lg:p-8">
+      <Head>
+        <title>Helio Meira de Souza Leles</title>
+      </Head>
+
+      <section className="hero h-screen flex justify-center items-center">
+        <div className="container mx-auto p-4 md:p-6 lg:p-8">
+          <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-gray-900 dark:text-white">
+            {lang.welcome}
+          </h1>
+          <p className="text-lg md:text-xl lg:text-2xl text-gray-600 dark:text-gray-400">
+            <br />
+            {lang.whoami}
+            <br /> <br />
+          </p>
+          <a
+            href="#my-projects"
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mr-4"
+          >
+            {lang.explore}
+          </a>
+          {changeLangBTN}
+        </div>
+      </section>
+
+      {/* About Section */}
+      <section className="about py-12">
+        <div className="container mx-auto p-4 md:p-6 lg:p-8">
+          <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold text-gray-900 dark:text-white">
+            {lang.title_aboutme}
+          </h2>
+          <p className="text-lg md:text-xl lg:text-2xl text-gray-600 dark:text-gray-400">
+            {lang.body_aboutme}
+          </p>
+        </div>
+      </section>
+
+      {/* Projects Section */}
+      <section className="projects py-12 w-fit" id="my-projects">
+        <div className="container mx-auto p-4 md:p-6 lg:p-8">
+          <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold text-gray-900 dark:text-white mb-8">
+            {lang.title_projects}
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-fit">
+            <a
+              className="project bg-white dark:bg-gray-800 rounded shadow-md w-fit"
+              href="https://github.com/NullClock/Ejectsoft-BETA"
+              target="_blank"
+            >
+              <div className="container mb-4">
+                <Image
+                  src="/ejsft.jpeg"
+                  alt="EjectSoft Logo"
+                  width={500}
+                  height={200}
+                />
+              </div>
+              <h3 className="text-lg font-bold text-gray-900 dark:text-white ml-4 mr-4 mb-4">
+                Ejectsoft (BETA)
+              </h3>
+              <p className="text-gray-600 dark:text-gray-400 ml-4 mr-4 mb-4">
+                {lang.desc_ejectsoft}
+              </p>
+            </a>
+            <a
+              className="project bg-white dark:bg-gray-800 rounded shadow-md"
+              href="https://pushsocket.github.io/"
+              target="_blank"
+            >
+              <div className="container mb-4">
+                <Image
+                  src="/pushsock.png"
+                  alt="PushSocket Logo"
+                  width={500}
+                  height={200}
+                />
+              </div>
+              <h3 className="text-lg font-bold text-gray-900 dark:text-white ml-4 mr-4 mb-4">
+                PushSocket (broken)
+              </h3>
+              <p className="text-gray-600 dark:text-gray-400 ml-4 mr-4 mb-4">
+                {lang.desc_pushsocket}
+              </p>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section className="contact py-12">
+        <div className="container mx-auto p-4 md:p-6 lg:p-8">
+          <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold text-gray-900 dark:text-white">
+            {lang.title_contact}
+          </h2>
+          <p className="text-lg md:text-xl lg:text-2xl text-gray-600 dark:text-gray-400">
+            {lang.body_contact}
+          </p>
+          <ul className="list-none">
+            <li className="mb-1">
+              <a
+                href="mailto:helio@xapktech.xyz"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                helio@xapktech.xyz
+              </a>
+            </li>
+            <li className="mb-1">
+              <a
+                href="https://github.com/NullClock"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                GitHub [NullClock]
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ app/ptbr.ts
@@ -0,0 +1,37 @@
+export const en = {
+  welcome: 'Welcome to my portfolio',
+  whoami:
+    "I'm Helio, a young developer in Philadelphia with a passion for Node.js.",
+  explore: 'Explore my work',
+  title_aboutme: 'About me',
+  body_aboutme:
+    "I'm a highly motivated and detail-oriented developer with 4 years of experience in Node.js and HTML/CSS. I'm passionate about coding and enjoy working on projects that challenge me to grow and learn.",
+  title_projects: 'My proejcts',
+  desc_ejectsoft:
+    'Ejectsoft is an in-beta Minecraft client that runs in the browser. (Eaglercraft-based)',
+  desc_pushsocket:
+    'PushSocket is a live updates service based on WebSockets. PushSocket facilitates the use of WebSockets and adds extra functionalities such as authentication. PushSocket is currently broken because the backend is down. I am currently working on finding a host for it.',
+  title_contact: 'Get in touch',
+  body_contact:
+    "If you'd like to collaborate or just say hello, feel free to reach out to me through the following channels:",
+  toggle: 'Mudar para Portugues',
+};
+
+export const pt = {
+  welcome: 'Bem-vindo ao meu portfolio',
+  whoami:
+    'Eu sou Helio, um jovem desenvolvedor em Filadélfia com paixão por Node.js.',
+  explore: 'Explore meu trabalho',
+  title_aboutme: 'Sobre mim',
+  body_aboutme:
+    'Eu sou um desenvolvedor altamente motivado e detalhista com 4 anos de experiência em Node.js e HTML/CSS. Eu sou apaixonado por programação e gosto de trabalhar em projetos que me desafiam a crescer e aprender.',
+  title_projects: 'Meus projetos',
+  desc_ejectsoft:
+    'Ejectsoft é um cliente Minecraft em beta que roda no navegador. (Baseado em Eaglercraft)',
+  desc_pushsocket:
+    'PushSocket é um serviço de atualizações em tempo real baseado em WebSockets. PushSocket facilita o uso de WebSockets e adiciona funcionalidades extras, como autenticação. PushSocket está atualmente quebrado porque os servidores estáo fora do ar. Estou atualmente trabalhando em encontrar um host para ele.',
+  title_contact: 'Fale comigo',
+  body_contact:
+    'Se você gostaria de colaborar ou apenas dizer olá, sinta-se à vontade para entrar em contato comigo através dos seguintes canais:',
+  toggle: 'Change to English',
+};
